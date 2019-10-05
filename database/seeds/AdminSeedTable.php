ef04d0e947f28d2b4252fa81054c84f6fb5b996d
FrancoisPagou <pagoufrancois@gmail.com>
2019-10-05 11:03:39 +0100

    adding asset, admin model created

---
+++ database/seeds/AdminSeedTable.php
@@ -0,0 +1,24 @@
+<?php
+
+use Faker\Factory as Faker;
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+
+class AdminSeedTable extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        Admin::create([
+            'name'      =>  $faker->name,
+            'email'     =>  'admin@admin.com',
+            'password'  =>  bcrypt('password'),
+        ]);
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+// use AdminSeedTable;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(AdminSeedTable::class);
     }
 }
