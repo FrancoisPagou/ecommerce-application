064a8bb7d8d8ae76c0783fc776ac4ea02b052ae3
FrancoisPagou <pagoufrancois@gmail.com>
2019-10-13 21:41:30 +0100

    BaseController created

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -2,9 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\FlashMessages;
 use Illuminate\Http\Request;
 
 class BaseController extends Controller
 {
-    //
+    use FlashMessages;
+
+    /**
+     * @var 
+     * 
+     */
+    protected $data = null;
+
+    /**
+     * @param string $title
+     * @param string $subtitle
+     *  
+     */
+    protected function setPageTitle($title, $subtitle)
+    {
+        view()->share(['pageTitle' => $title, 'subTitle' => $subtitle]);
+    }
+
+    /**
+     * @param int $errorCode
+     * @param null $message
+     * @return \Illuminate\Http\Response
+     */
+    protected function showErrorPage($errorCode = 404, $message = null)
+    {
+        $data['message'] = $message;
+        return response()->view('errors.'.$errorCode, $data, $errorCode);
+    }
+
+    /**
+     * @param bool $error
+     * @param int $responseCode
+     * @param array $message
+     * @param $data
+     * @return \Illuminate\Http\JsonResponse
+     */
+    protected function responseJson($error = true, $responseCode = 200, $message = [], $data = null)
+    {
+        return response()->json([
+            'error' => $error,
+            'response_code' => $responseCode,
+            'message' => $message,
+            'data' => $data
+        ]);
+    }
 }
