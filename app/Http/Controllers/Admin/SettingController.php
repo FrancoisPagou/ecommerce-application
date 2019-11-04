7714a7d630a13ea07ecd785868274e99fdb260bd
FrancoisPagou <pagoufrancois@gmail.com>
2019-11-04 21:27:07 +0100

    upload file trait

--- app/Http/Controllers/Admin/SettingController.php
+++ app/Http/Controllers/Admin/SettingController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Traits\UploadAble;
 use Illuminate\Http\Request;
 use App\Http\Controllers\BaseController;
 
 class SettingController extends BaseController
 {
+    use UploadAble;
+    
     public function index()
     {
         $this->setPageTitle('Settings', 'Manage Settings');
--- app/Traits/UploadAble.php
+++ app/Traits/UploadAble.php
@@ -17,4 +17,23 @@ trait UploadAble{
      * @param string $filename
      * @return string|false
      */
+    public function uploadOne(UploadedFile $file, $folder = null, $disk = 'public', $filename = null)
+    {
+        $name = !is_null($filename) ? $filename : str_random(25);
+
+        return $file->storeAs(
+            $folder,
+            $name . "." . $file->getClientOriginalExtension(),
+            $disk
+        );
+    }
+
+    /**
+     * @param null $path
+     * @param string $disk
+     */
+    public function deleteOne($path = null, $disk = 'public')
+    {
+        Storage::disk($disk)->delete($path);
+    }
 }
